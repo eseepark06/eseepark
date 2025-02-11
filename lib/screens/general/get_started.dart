74f6a257f9eba17bef58e6f2549c037b28d4f142
sharraine <qskvcueto@tip.edu.ph>
2025-02-11 11:42:47 +0800

    v1.0.4 (SKVC)

--- lib/screens/general/get_started.dart
+++ lib/screens/general/get_started.dart
@@ -25,17 +25,17 @@ class _GetStartedState extends State<GetStarted> {
 
   final getStartedInfos = [
     GetStartedInfo(
-        title: 'Find Parking in Seconds',
+        title: 'Find Parking\nIn Seconds',
         subtitle: 'No more aimless searching, see available parking slots instantly',
         image: 'assets/images/getstarted/slide1.png'
     ),
     GetStartedInfo(
-        title: 'Real-Time Parking Updates',
+        title: 'Real-Time\nParking Updates',
         subtitle: 'Stay updated on parking status at a glance—no more aimless searching!',
         image: 'assets/images/getstarted/slide1.png'
     ),
     GetStartedInfo(
-        title: 'Know Before You Go',
+        title: 'Know Before\nYou Go',
         subtitle: 'Save time by checking available slots before heading out.',
         image: 'assets/images/getstarted/slide1.png'
     ),
@@ -57,14 +57,14 @@ class _GetStartedState extends State<GetStarted> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             SizedBox(
-              height: screenHeight * 0.7,
+              height: screenHeight * 0.65,
               child: Column(
                 children: [
                   Expanded(
                     child: Container(
                       margin: EdgeInsets.only(
-                          left: screenWidth * .05,
-                          right: screenWidth * .05
+                          left: screenWidth * .07,
+                          right: screenWidth * .07
                       ),
                       child: SizedBox(
                         width: screenWidth * 0.9,
@@ -81,7 +81,7 @@ class _GetStartedState extends State<GetStarted> {
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold,
-                                    fontSize: screenSize * 0.026
+                                    fontSize: screenSize * 0.03
                                 ),
                               ),
                             )
@@ -104,14 +104,20 @@ class _GetStartedState extends State<GetStarted> {
                   SizedBox(height: screenHeight * .03),
                   Expanded(
                     child: Container(
-                      color: Colors.orange,
                       padding: EdgeInsets.symmetric(
                         horizontal: screenWidth * .05
                       ),
-                      child: Text(getStartedInfos[currentIndex].subtitle,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: screenSize * 0.014
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 200),
+                        transitionBuilder: (Widget child, Animation<double> animation) {
+                          return FadeTransition(opacity: animation, child: child);
+                        },
+                        child: Text(getStartedInfos[currentIndex].subtitle,
+                          key: ValueKey<String>(getStartedInfos[currentIndex].subtitle),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: screenSize * 0.014
+                          ),
                         ),
                       ),
                     ),
@@ -120,9 +126,10 @@ class _GetStartedState extends State<GetStarted> {
               ),
             ),
             Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: screenWidth * .06,
-                vertical: screenHeight * .05
+              padding: EdgeInsets.only(
+                left: screenWidth * .06,
+                right: screenWidth * .06,
+                bottom: screenHeight * .12
               ),
               width: screenWidth,
               child: ElevatedButton(
@@ -131,16 +138,21 @@ class _GetStartedState extends State<GetStarted> {
                     setState(() {
                       currentIndex++;
                     });
+                  } else {
+                    setState(() {
+                      currentIndex--;
+                    });
                   }
                 },
                 style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(30),
                   ),
+                  elevation: 0,
                   padding: EdgeInsets.symmetric(
                     vertical: screenHeight * 0.015
                   ),
-                  backgroundColor: Color.lerp(Theme.of(context).colorScheme.primary, Colors.deepOrange, 0.6)
+                  backgroundColor: Color.lerp(Theme.of(context).colorScheme.primary, Colors.deepOrange, 0.4)
                 ),
                 child: AnimatedSwitcher(
                   duration: const Duration(milliseconds: 200),
