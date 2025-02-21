b83c07a996fb1f5f65e1de23268a1e6b60469ed7
sharraine <qskvcueto@tip.edu.ph>
2025-02-21 16:26:38 +0800

    v1.0.7 (SKVC)

--- lib/screens/others/search/search.dart
+++ lib/screens/others/search/search.dart
@@ -4,6 +4,7 @@ import 'package:eseepark/globals.dart';
 import 'package:eseepark/models/establishment_model.dart';
 import 'package:eseepark/models/search_model.dart';
 import 'package:eseepark/providers/root_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -20,12 +21,52 @@ class _SearchState extends State<Search> {
   final EstablishmentController controller = EstablishmentController();
   final TextEditingController searchController = TextEditingController();
 
+  List<Establishment> searchedData = [];
+  bool isSearching = false;
+
   @override
   void dispose() {
     super.dispose();
 
     searchController.dispose();
   }
+
+  TextSpan _highlightText(String text, String searchText) {
+    List<TextSpan> spans = [];
+
+    String lowerText = text.toLowerCase();
+    String lowerSearchText = searchText.toLowerCase();
+
+    int start = 0;
+    int index = lowerText.indexOf(lowerSearchText);
+
+    while (index != -1) {
+      if (index > start) {
+        spans.add(TextSpan(
+          text: text.substring(start, index),
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ));
+      }
+
+      spans.add(TextSpan(
+        text: text.substring(index, index + searchText.length),
+        style: TextStyle(fontWeight: FontWeight.normal, color: Colors.black),
+      ));
+
+      start = index + searchText.length;
+      index = lowerText.indexOf(lowerSearchText, start);
+    }
+
+    if (start < text.length) {
+      spans.add(TextSpan(
+        text: text.substring(start),
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+      ));
+    }
+
+    return TextSpan(children: spans);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +86,18 @@ class _SearchState extends State<Search> {
                 controller: searchController,
                 onChanged: (val) async {
                   setState(() {
-
+                    searchedData.clear();
                   });
                  if(val.trim().isNotEmpty) {
-                   final data = await controller.searchEstablishments(searchText: val, maxResults: 10);
+                   setState(() {
+                     isSearching = true;
+                   });
+                   searchedData = await controller.searchEstablishments(searchText: val, maxResults: 10);
 
-                   print(data.length);
+                   // await Future.delayed(Duration(seconds: 2));
+                   setState(() {
+                     isSearching = false;
+                   });
                  }
                 },
                 call: (val) {
@@ -94,19 +141,99 @@ class _SearchState extends State<Search> {
         ),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.05
-        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: screenHeight * 0.014),
             if(searchController.text.trim().isNotEmpty)
-             Text('Search for "${searchController.text.trim()}"',
-               maxLines: 1,
-               overflow: TextOverflow.ellipsis,
-               style: TextStyle(
-                 fontWeight: FontWeight.w600,
+             Container(
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Padding(
+                     padding: EdgeInsets.symmetric(
+                         horizontal: screenWidth * 0.05
+                     ),
+                     child: Text('Search for "${searchController.text.trim()}"',
+                       maxLines: 1,
+                       overflow: TextOverflow.ellipsis,
+                       style: TextStyle(
+                         fontWeight: FontWeight.w600,
+                       ),
+                     ),
+                   ),
+                   Container(
+                     alignment: Alignment.center,
+                     padding: EdgeInsets.only(top: screenHeight * 0.005),
+                     child: !isSearching ?
+                      ListView.builder(
+                       itemCount: searchedData.length,
+                       shrinkWrap: true,
+                       physics: const NeverScrollableScrollPhysics(),
+                       itemBuilder: (context, index) {
+                         final establishment = searchedData[index];
+                         return InkWell(
+                           onTap: () {
+                             print('Going');
+                           },
+                           child: Container(
+                             margin: EdgeInsets.only(top: screenHeight * 0.01, bottom: screenHeight * 0.01),
+                             padding: EdgeInsets.symmetric(
+                                 horizontal: screenWidth * 0.05
+                             ),
+                             child: Row(
+                               children: [
+                                 Container(
+                                   height: screenWidth * 0.1,
+                                   width: screenWidth * 0.1,
+                                   decoration: BoxDecoration(
+                                     border: Border.all(
+                                       color: Theme.of(context).colorScheme.secondary,
+                                       width: 1,
+                                     ),
+                                     borderRadius: BorderRadius.circular(8),
+                                   ),
+                                   padding: EdgeInsets.all(screenWidth * 0.021),
+                                   child: SvgPicture.asset(
+                                     establishment.establishmentType == 'Mall'
+                                         ? 'assets/svgs/home/malls.svg'
+                                         : establishment.establishmentType == 'Outdoor'
+                                         ? 'assets/svgs/home/outdoor.svg'
+                                         : '',
+                                     width: screenWidth * 0.04,
+                                   ),
+                                 ),
+                                 SizedBox(width: screenWidth * 0.04),
+                                 Container(
+                                   child: Column(
+                                     crossAxisAlignment: CrossAxisAlignment.start,
+                                     children: [
+                                       RichText(
+                                         text: _highlightText(establishment.name, searchController.text.trim()),
+                                         maxLines: 1,
+                                         overflow: TextOverflow.ellipsis,
+                                       ),
+                                       Text(establishment.establishmentType,
+                                         style: TextStyle(
+                                             color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.4),
+                                             fontSize: screenWidth * 0.025
+                                         ),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                         );
+                       },
+                     ) :
+                      Padding(
+                        padding: EdgeInsets.only(top: screenHeight * 0.03),
+                        child: CupertinoActivityIndicator(),
+                      )
+                   )
+                 ],
                ),
              ),
             SizedBox(height: screenHeight * 0.01),
@@ -114,10 +241,15 @@ class _SearchState extends State<Search> {
               child: rootProvider.getGeneralProvider.searched.isNotEmpty && searchController.text.trim().isEmpty ? Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Recent Searches',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: screenSize * 0.013
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.05
+                    ),
+                    child: Text('Recent Searches',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: screenSize * 0.013
+                      ),
                     ),
                   ),
                   ListView.builder(
@@ -127,30 +259,51 @@ class _SearchState extends State<Search> {
                     itemBuilder: (context, index) {
                       final search = rootProvider.getGeneralProvider.searched[index];
 
-                      return Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: screenHeight * 0.013
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.history),
-                                SizedBox(width: screenWidth * 0.04),
-                                Text(search.searchText, maxLines: 1, overflow: TextOverflow.ellipsis,)
-                              ],
-                            ),
-                            InkWell(
-                              onTap: () {
-                                rootProvider.getGeneralProvider.deleteSearch(search);
-                                setState(() {
+                      return InkWell(
+                        onTap: () async {
+                          searchController.text = search.searchText;
 
-                                });
-                              },
-                              child: Icon(Icons.close)
-                            )
-                          ],
+                          FocusScope.of(context).unfocus();
+
+                          setState(() {
+                            isSearching = true;
+                          });
+
+                          searchedData = await controller.searchEstablishments(
+                              searchText: search.searchText,
+                              maxResults: 10
+                          );
+
+                          setState(() {
+                            isSearching = false;
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.013,
+                            horizontal: screenWidth * 0.05
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.history),
+                                  SizedBox(width: screenWidth * 0.04),
+                                  Text(search.searchText, maxLines: 1, overflow: TextOverflow.ellipsis,)
+                                ],
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  rootProvider.getGeneralProvider.deleteSearch(search);
+                                  setState(() {
+                        
+                                  });
+                                },
+                                child: Icon(Icons.close)
+                              )
+                            ],
+                          ),
                         ),
                       );
                     },
