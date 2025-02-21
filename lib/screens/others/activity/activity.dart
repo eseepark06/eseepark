f26fb25d915a254ca8c7e0b53d93f8d5bc4a1ffc
sharraine <qskvcueto@tip.edu.ph>
2025-02-21 17:04:15 +0800

    v1.0.7 (SKVC)

--- lib/screens/others/activity/activity.dart
+++ lib/screens/others/activity/activity.dart
@@ -1,3 +1,4 @@
+import 'package:eseepark/globals.dart';
 import 'package:flutter/material.dart';
 
 class Activity extends StatefulWidget {
@@ -11,7 +12,42 @@ class _ActivityState extends State<Activity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Activity',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
 
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                elevation: 0
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.history,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: screenWidth * 0.02),
+                  Text('History',
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
